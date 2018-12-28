a917aea73cc7e3512c4ff0cfe8bc279db38c37c4
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-12-28 17:05:26 +0330

    course id changed to char

--- csecourses/models.py
+++ csecourses/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class CSECourse(models.Model):
+    cse_id = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+
+
+class CSETerm(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()
