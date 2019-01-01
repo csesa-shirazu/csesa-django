f0f92a91a55c43558e877f31c27b4c7c43c9731d
Ali Mahdiyar <alimahdiyar77@gmail.com>
2019-01-01 16:32:20 +0330

    add graders

--- csesa/views.py
+++ csesa/views.py
@@ -1,9 +1,11 @@
+from django.contrib.contenttypes.models import ContentType
 from django.http import Http404
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from campaigns.models import CampaignPartyRelation, CampaignPartyRelationType
-from campaigns.serializers import GraderRelationSerializer
+from campaigns.models import CampaignPartyRelation, CampaignPartyRelationType, Campaign
+from campaigns.serializers import GraderRelationSerializer, CampaignAsCourseSimpleSerializer
+from users.models import User, Profile
 
 
 def index_view(request):
@@ -16,13 +18,45 @@ def index_view(request):
 
 def graders_view(request):
     if request.user.is_authenticated and (request.user.is_staff or request.user.is_superuser):
-        context = {
+        context = {}
+        if 'gid' in request.POST:
+            try:
+                the_grader = User.objects.get(username=request.POST['gid']).profile.first()
+            except User.DoesNotExist:
+                context['message'] = 'پروفایل با این یوزرنیم وجود ندارد'
+            else:
+                try:
+                    the_campaign = Campaign.objects.get(id=request.POST['cid'])
+                except Campaign.DoesNotExist:
+                    context['message'] = 'درس وارد شده نامعتبر است'
+                else:
+                    if CampaignPartyRelation.objects.filter(
+                        campaign = the_campaign,
+                        content_type = ContentType.objects.get_for_model(the_grader),
+                        object_id = the_grader.id,
+                        type=CampaignPartyRelationType.GRADER
+                    ).exists():
+                        context['message'] = 'گریدر از قبل وارد شده'
+                    else:
+                        CampaignPartyRelation.objects.create(
+                            campaign = the_campaign,
+                            content_type = ContentType.objects.get_for_model(the_grader),
+                            object_id = the_grader.id,
+                            type=CampaignPartyRelationType.GRADER
+                        )
+                        context['message'] = 'با موفقیت ثبت شد'
+
+        context.update({
             'gcrs': GraderRelationSerializer(
                 CampaignPartyRelation.objects.filter(
                     type = CampaignPartyRelationType.GRADER
                 ), many=True
+            ).data,
+            'courses': CampaignAsCourseSimpleSerializer(
+                Campaign.objects.all(),
+                many=True
             ).data
-        }
+        })
         return render(request, "graders.html", context)
     raise Http404
 
