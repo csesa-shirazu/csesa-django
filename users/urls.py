9fcc664b24e005e4c39472d016550fc5322aa9f8
Ali Mahdiyar <alimahdiyar77@gmail.com>
2019-06-16 01:24:24 +0430

    logout and login view

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
-    # path('login/', views.login_view, name='login'),
-    # path('logout/', views.logout_view, name='logout'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('api-token-auth/', obtain_auth_token, name='api-token-auth'),  # <-- And here
 ]
